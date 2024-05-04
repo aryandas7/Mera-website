99376649629cfbb6643479e3821572a0f9e7e55e
aryandas7 <167899635+aryandas7@users.noreply.github.com>
2024-05-04 14:41:11 +0000

    added the Flask project

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-#print("Hello, World")
-#xxxx
-#print(__name__)
+from flask import Flask
 
-#xxxx
-
-#from flask import Flask
-#app=Flask(__name__)
-#@app.route("/")
-#def hello_world():
-#  return "Hello, Aryan"
-
-#print(__name__)
-#if __name__=="__main__":
-#  print("I am inside if now")
-
-#if __name__=="__main__":
-#  app.run(host="0.0.0.0",debug=True)
-#xxxx
-from flask import Flask, render_template
 app=Flask(__name__)
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
-
-print(__name__)
-if __name__=="__main__":
-  print("I am inside if now")
-
+    return "Hello, Aryan"
+  
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
-
-#xxxx
